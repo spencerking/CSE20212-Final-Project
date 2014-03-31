114977b68ad8f9bd052b22f1d19f3cd767a02bf9
spencerking <sking8@nd.edu>
2014-03-31 16:36:22 -0400

    Started working on the heightfield
    I have gone through the first three swiftless tutorials on terrain
    generation. They generally make sense so far. The code I have written
    is essentially the same as what is on swiftless.com

---
+++ heightfield.cpp
@@ -0,0 +1,45 @@
+//
+//  heightfield.cpp
+//  
+//
+//  Created by Spencer King on 3/31/14.
+//
+//
+
+#include <stdio.h>
+#include <gl/gl.h>
+#include "heightfield.h"
+
+
+bool HeightField::Create(char *hFileName, int hWidth, const int hHeight){
+    
+    
+    hmHeight=hHeight;
+    hmWidth=hWidth;
+    
+    FILE *fp;
+    fp = fopen(hFileName, "rb");
+    fread(hHeightField, 1, hWidth * hHeight, fp);
+    fclose(fp);
+    for (int hMapX = 0; hMapX<hWidth; hMapX++){
+        for (int hMapZ=0; hMapZ<hHeight; hMapZ++){
+            printf("%d, %d, %d\n", hMapX, hMapZ, hHeightField[hMapX][hMapZ]);
+        }
+    }
+    return true;
+    
+}
+
+void HeightField::Render(void){
+    
+    //this will render the terrain as points in space
+    //points are created on the X and Z axes at the appropriate Y positions
+    glBegin(GL_POINTS);
+    for (int hMapX=0; hMapX<hmWidth; hMapX++){
+        for (int hMapZ=0; hMapZ<hmHeight; hMapZ++){
+            glVertex3f(hMapX, hHeightField[hMapX][hMapZ], hMapZ);
+        }
+    }
+    glEnd();
+    
+}
---
+++ heightfield.h
@@ -0,0 +1,30 @@
+//
+//  heightfield.h
+//  
+//
+//  Created by Spencer King on 3/31/14.
+//
+//
+
+#ifndef ____heightfield__
+#define ____heightfield__
+
+#include <iostream>
+//#include <windows.h>
+//windows.h is needed for the BYTE variable type to load in the heightfield.raw image file, we need to either find a replacement for the header or define BYTE ourselves
+
+class HeightField {
+    
+public:
+    bool Create(char *hFileName, int hWidth, const int hHeight);
+    void Render(void);
+    BYTE hhEIGHTfIELD[1024][1024];
+    
+private:
+    //these hold the heigh and width of the height field
+    int hmHeight;
+    int hmWidth;
+    
+};
+
+#endif /* defined(____heightfield__) */
